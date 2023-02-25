9befe752dc85b4250d796cf3ce019258f2829160
carreb <caleb@domyjobfor.me>
2023-02-24 22:35:43 -0500

    setup index

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 45678;
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config()
+
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true })
+
+const db = mongoose.connection
+db.on('error', (err) => {
+    console.error(err);
+    console.log("DATABASE FAILURE: Read above for details.")
+})
+
+db.once('open', () => {
+    console.log("Mongoose connected successfully!")
+})
+
+app.use(express.json())
+app.use(cors())
+
+
+// routes
+const slayer = require("./routes/slayer.js")
